ec77f23fe5ccced3afd8891feaa04a84ba4eb40d
DanViTrinh <danvitrinh@outlook.com>
2024-05-06 21:40:36 +0200

    removed unused comment

--- frontend/src/components/SingleGrade.tsx
+++ frontend/src/components/SingleGrade.tsx
@@ -45,34 +45,6 @@ export default function SingleGrade({
     let userInput = event.target.value
     const updatedGrade: Grade = { ...grade, GradePoint: userInput }
     updateGradesWithNewGrade(updatedGrade);
-
-    // let valid = false
-
-    // check if it is valid number
-    // let gradePointNum = Number(userInput);
-    // if (!isNaN(gradePointNum) && gradePointNum >= 0 && gradePointNum <= 5) {
-    //   valid = true
-    //   userInput = String(gradePointNum)
-    // }
-
-
-    // if (!valid) {
-    //   // check if it is a valid character
-    //   const validGradeCharRegex = /^[a-fA-F]?/g;
-    //   const validGrade = userInput.match(validGradeCharRegex)
-    //   console.log(validGrade)
-    //   if (validGrade !== null && validGrade[0] !== "") {
-    //     userInput = validGrade[0]
-    //     valid = true
-    //   }
-    // }
-
-    // if (valid || userInput === '') {
-    //   const updatedGrade: Grade = { ...grade, GradePoint: userInput }
-    //   updateGradesWithNewGrade(updatedGrade);
-    // }
-
-    // const gradeWithoutEmptySpaces = userInput.replace(/\s+/g, '')
   }
 
   function removeGrade(): void {
